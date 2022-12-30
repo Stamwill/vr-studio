c4c5efdae497fa38f7da07bb4b378cd79c8c9c88
William Stam <stam@williams-mbp.home>
2022-12-30 13:41:35 +0000

    [api] Add mock data to use

---
+++ src/api/mock.tsx
@@ -0,0 +1,117 @@
+export const menuPrimary = [
+  {
+    label: 'About',
+    url: '#',
+  },
+  {
+    label: 'Careers',
+    url: '#',
+  },
+  {
+    label: 'Events',
+    url: '#',
+  },
+  {
+    label: 'Products',
+    url: '#',
+  },
+  {
+    label: 'Support',
+    url: '#',
+  },
+]
+
+export const heroData = [
+  {
+    heroBackground: {
+      mobile: './images/mobile/Hero.jpg',
+      desktop: './images/desktop/Hero.jpg',
+    },
+    title: 'Immersive Experiences that deliver',
+  },
+]
+
+export const contentBlock = [
+  {
+    contentImg: {
+      mobile: './images/mobile/Interactive.jpg',
+      desktop: './images/desktop/Interactive.jpg',
+    },
+    title: 'The leader in interactive vr',
+    text: 'Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.',
+  },
+]
+
+export const creationGrid = [
+  {
+    title: 'Our creations',
+  },
+  {
+    gridContent: [
+      {
+        label: 'deep earth',
+        url: '#',
+        contentImg: {
+          mobile: './images/mobile/DeepEarth.jpg',
+          desktop: './images/desktop/DeepEarth.jpg',
+        },
+      },
+      {
+        label: 'night arcade',
+        url: '#',
+        contentImg: {
+          mobile: './images/mobile/NightArcade.jpg',
+          desktop: './images/desktop/NightArcade.jpg',
+        },
+      },
+      {
+        label: 'football team vr',
+        url: '#',
+        contentImg: {
+          mobile: './images/mobile/footballTeam.jpg',
+          desktop: './images/desktop/footballTeam.jpg',
+        },
+      },
+      {
+        label: 'the grid',
+        url: '#',
+        contentImg: {
+          mobile: './images/mobile/grid.jpg',
+          desktop: './images/desktop/grid.jpg',
+        },
+      },
+      {
+        label: 'from up above vr',
+        url: '#',
+        contentImg: {
+          mobile: './images/mobile/fromAbove.jpg',
+          desktop: './images/desktop/fromAbove.jpg',
+        },
+      },
+      {
+        label: 'pocket borealis',
+        url: '#',
+        contentImg: {
+          mobile: './images/mobile/pocketBorealis.jpg',
+          desktop: './images/desktop/pocketBorealis.jpg',
+        },
+      },
+      {
+        label: 'the curiosity',
+        url: '#',
+        contentImg: {
+          mobile: './images/mobile/curiosity.jpg',
+          desktop: './images/desktop/curiosity.jpg',
+        },
+      },
+      {
+        label: 'make it fisheye',
+        url: '#',
+        contentImg: {
+          mobile: './images/mobile/fishEye.jpg',
+          desktop: './images/desktop/fishEye.jpg',
+        },
+      },
+    ],
+  },
+]
