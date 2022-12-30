1a4da62790dc352322644e04531501b722ca0bfc
William Stam <stam@williams-mbp.home>
2022-12-30 13:41:18 +0000

    [ContentBlock] Create ContentBlock base

---
+++ src/blocks/ContentBlock/ContentBlock.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import classes from './contentBlock.module.css'
+
+const ContentBlock = () => {
+  return (
+    <section className={classes.root}>
+      <h2>Content Block</h2>
+    </section>
+  )
+}
+
+export default ContentBlock
