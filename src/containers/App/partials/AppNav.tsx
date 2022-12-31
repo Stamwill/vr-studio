7d45f4fbb2cec54c4fccd1adbb1c7b85ff02a8f0
William Stam <stam@williams-mbp.home>
2022-12-31 12:36:09 +0000

    [AppNav] Add AppNav

---
+++ src/containers/App/partials/AppNav.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import classes from './AppNav.module.css'
+
+interface Navigation {
+  label: string
+  url: string
+}
+
+interface NavigationProps {
+  menu: Navigation[]
+}
+
+const AppNav = (props: NavigationProps) => {
+  const { menu } = props
+  return (
+    <section className={classes.root}>
+      {menu.map((item: Navigation, idx: number) => (
+        <a href={item.url} key={idx}>
+          {item.label}
+        </a>
+      ))}
+    </section>
+  )
+}
+
+export default AppNav
