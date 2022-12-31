05b364085af05941322336ef9ef1ade27ee15a5e
William Stam <stam@williams-mbp.home>
2022-12-31 12:36:27 +0000

    [App] Add imports

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react'
+import { menuPrimary, contentBlock } from 'api/mock'
 import ContentBlock from 'blocks/ContentBlock'
 import classes from './App.module.css'
+import AppNav from './partials/AppNav'
 
 const App = () => {
   return (
     <div className={classes.root}>
       <header>
         <title>VR Studio</title>
-        <span>logo</span>
-        <h2>AppNav</h2>
+        <AppNav menu={menuPrimary} />
       </header>
 
       <main>
-        <ContentBlock />
+        <ContentBlock content={contentBlock} />
       </main>
 
       <footer>
