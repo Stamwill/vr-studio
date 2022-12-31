f1d59f711306e4b15a9ca96f4a0c3eea6ab3f1ad
William Stam <stam@williams-mbp.home>
2022-12-31 12:35:41 +0000

    [ContentBlock] Add props and styling

--- src/blocks/ContentBlock/ContentBlock.tsx
+++ src/blocks/ContentBlock/ContentBlock.tsx
@@ -1,10 +1,48 @@
 import * as React from 'react'
+import Image from 'next/image'
 import classes from './contentBlock.module.css'
 
-const ContentBlock = () => {
+interface Content {
+  contentImg: {
+    mobile: string
+    desktop: string
+  }
+  title: string
+  text: string
+}
+
+interface ContentProps {
+  content: Content[]
+}
+
+const ContentBlock = (props: ContentProps) => {
+  const { content } = props
   return (
     <section className={classes.root}>
-      <h2>Content Block</h2>
+      {content.map((item, idx) => (
+        <div className={classes.container} key={idx}>
+          <div className={classes.imgContainer}>
+            <picture className={classes.imgContainer}>
+              <source
+                className={classes.mobile}
+                media="(max-width: 768px)"
+                srcSet={item.contentImg.mobile}
+              />
+              <source
+                className={classes.tablet}
+                media="(max-width: 1200px)"
+                srcSet={item.contentImg.desktop}
+              />
+              <img className={classes.desktop} src={item.contentImg.desktop} alt="space" />
+            </picture>
+          </div>
+
+          <div className={classes.textContainer}>
+            <h2 className={classes.header}>{item.title}</h2>
+            <p className={classes.text}>{item.text}</p>
+          </div>
+        </div>
+      ))}
     </section>
   )
 }
