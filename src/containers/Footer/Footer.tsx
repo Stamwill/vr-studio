5f2c685af3ad4bf386dd83b06b61b797969f3ac8
William Stam <stam@williams-mbp.home>
2023-01-06 11:07:41 +0000

    [Footer] Import Socialmedias and add styling

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
+import { logo, socialMedias } from 'api/mock'
 import classes from './Footer.module.css'
+import SocialMedias from 'components/SocialMedias'
 
 interface Navigation {
   label: string
@@ -14,12 +16,23 @@ const Footer = (props: NavigationProps) => {
   const { menu } = props
   return (
     <section className={classes.root}>
+      <img src={logo} alt="company logo" />
       <div className={classes.navBar}>
         {menu.map((item: Navigation, i: number) => (
           <a className={classes.label} href={item.url} key={i}>
             {item.label}
           </a>
         ))}
+      </div>
+
+      <div className={classes.socialMedias}>
+        {socialMedias.map((item, i) => (
+          <SocialMedias key={i} {...item} />
+        ))}
+      </div>
+
+      <div className={classes.copyright}>
+        <p>© 2021 Loopstudios. All rights reserved.</p>
       </div>
     </section>
   )
