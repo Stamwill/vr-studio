5f490e9e35d28e5b6721192fc25b94a4516fe5f8
William Stam <stam@williams-mbp.home>
2023-01-09 15:36:15 +0000

    [AppNav] Add logo container

--- src/containers/App/partials/AppNav.tsx
+++ src/containers/App/partials/AppNav.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import classnames from 'clsx'
+import Image from 'next/image'
 import { logo } from 'api/mock'
 import Hamburger from 'components/Hamburger'
 import classes from './AppNav.module.css'
@@ -19,7 +20,9 @@ const AppNav = (props: NavigationProps) => {
   const { menu, open, toggleMenu } = props
   return (
     <section className={classnames(classes.root, { [classes.menuIsOpen]: open })}>
-      <img className={classes.logo} src={logo} alt="company logo" />
+      <div className={classes.logoContainer}>
+        <Image src={logo} fill alt="company logo" />
+      </div>
       <div className={classes.navBar}>
         {menu.map((item: Navigation, idx: number) => (
           <a className={classes.label} key={idx}>
