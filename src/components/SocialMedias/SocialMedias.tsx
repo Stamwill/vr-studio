f1a7d47d13c7a6c80dbf32fea87a15c4ed79720c
William Stam <stam@williams-mbp.home>
2023-01-10 09:46:18 +0000

    [SocialMedias] Replace img with Image

--- src/components/SocialMedias/SocialMedias.tsx
+++ src/components/SocialMedias/SocialMedias.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import Image from 'next/image'
 import classes from './SocialMedias.module.css'
 
 interface Item {
@@ -10,9 +11,11 @@ const SocialMedias = (props: Item) => {
   const { url, img } = props
   return (
     <div className={classes.root}>
-      <a href={url}>
-        <img src={img} alt="item text" />
-      </a>
+      <div>
+        <a href={url} target="_blank" rel="noreferrer">
+          <Image src={img} alt={url} fill />
+        </a>
+      </div>
     </div>
   )
 }
