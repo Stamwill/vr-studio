972c24036147be3fb02a11cb73f5fca0bd2be551
William Stam <stam@williams-mbp.home>
2023-01-10 10:44:42 +0000

    [SocialMedias] Add props for Image

--- src/components/SocialMedias/SocialMedias.tsx
+++ src/components/SocialMedias/SocialMedias.tsx
@@ -5,15 +5,16 @@ import classes from './SocialMedias.module.css'
 interface Item {
   url: string
   img: string
+  alt: string
 }
 
 const SocialMedias = (props: Item) => {
-  const { url, img } = props
+  const { url, img, alt } = props
   return (
     <div className={classes.root}>
       <div>
         <a href={url} target="_blank" rel="noreferrer">
-          <Image src={img} alt={url} fill />
+          <Image src={img} alt={alt} fill />
         </a>
       </div>
     </div>
