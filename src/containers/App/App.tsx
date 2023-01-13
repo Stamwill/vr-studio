c6d51936874744dccf2a1173c9b777890a8dae4c
William Stam <stam@williams-mbp.home>
2023-01-13 12:52:34 +0000

    [App] Remove unused components

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,27 +1,15 @@
 import * as React from 'react'
-import { menuPrimary, contentBlock, heroData } from 'api/mock'
-import AppNav from './partials/AppNav'
-import AppDrawer from './partials/AppDrawer'
+import { contentBlock, heroData } from 'api/mock'
 import Hero from 'blocks/Hero'
 import Introduction from 'blocks/IntroductionBlock'
 import Grid from 'containers/Grid'
-import Footer from 'containers/Footer'
 import classes from './App.module.css'
 
 const App = () => {
-  const [menuIsOpen, setMenuOpen] = React.useState(false)
-
-  const toggleMenu = () => {
-    setMenuOpen((prevState) => !prevState)
-    console.log(menuIsOpen)
-  }
-
   return (
     <div className={classes.root}>
       <header>
         <title>VR Studio</title>
-        <AppNav menu={menuPrimary} open={menuIsOpen} toggleMenu={toggleMenu} />
-        <AppDrawer menu={menuPrimary} toggleMenu={toggleMenu} open={menuIsOpen} />
         <Hero content={heroData} />
       </header>
 
@@ -29,8 +17,6 @@ const App = () => {
         <Introduction content={contentBlock} />
         <Grid />
       </main>
-
-      <Footer menu={menuPrimary} />
     </div>
   )
 }
