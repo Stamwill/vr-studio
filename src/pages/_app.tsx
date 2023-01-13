d9d2e10dc9a2b88953566551ec0d7a575f7ede5a
William Stam <stam@williams-mbp.home>
2023-01-13 12:54:43 +0000

    [_app] Add GlobalProvider, import navbar and footer, add state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,51 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import * as React from 'react'
+import Head from 'next/head'
+import type { AppProps } from 'next/app'
+import { menu } from 'api/mock'
+import { GlobalProvider } from 'api/GlobalContext'
+import AppNav from 'containers/App/partials/AppNav'
+import AppDrawer from 'containers/App/partials/AppDrawer'
+import Footer from 'containers/Footer'
+import '../styles/globals.css'
+
+interface GlobalProps {
+  Component: any
+  ctx: []
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [menuIsOpen, setMenuOpen] = React.useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen((prevState) => !prevState)
+  }
+
+  return (
+    <GlobalProvider>
+      <Head>
+        <title>VR Studio</title>
+      </Head>
+      <header>
+        <AppNav menu={menu} open={menuIsOpen} toggleMenu={toggleMenu} />
+        <AppDrawer menu={menu} open={menuIsOpen} toggleMenu={toggleMenu} />
+      </header>
+
+      <Component {...pageProps} />
+      <Footer menu={menu} />
+    </GlobalProvider>
+  )
+}
+
+App.getInitialProps = async (props: GlobalProps) => {
+  const { Component, ctx } = props
+
+  let pageProps = {}
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+
+  return {
+    pageProps,
+  }
 }
