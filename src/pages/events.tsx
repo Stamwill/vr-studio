297944dcdfe239d9034583c8b29982129cb4beeb
William Stam <stam@williams-mbp.home>
2023-01-30 12:22:14 +0000

    [Pages] Add Pages to be worked upon

---
+++ src/pages/events.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import classes from 'styles/about.module.css'
+
+export default function Events() {
+  return (
+    <main className={classes.aboutMain}>
+      <div className={classes.root}>
+        <p>Events</p>
+      </div>
+    </main>
+  )
+}
---
+++ src/pages/support.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import classes from 'styles/about.module.css'
+
+export default function Support() {
+  return (
+    <main className={classes.aboutMain}>
+      <div className={classes.root}>
+        <p>Support</p>
+      </div>
+    </main>
+  )
+}
