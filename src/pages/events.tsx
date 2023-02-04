ed402a97835c24fa1327de44b3a735eab5c93589
William Stam <stam@williams-mbp.home>
2023-02-04 16:41:02 +0000

    [events] Add data and import Event component

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react'
-import classes from 'styles/about.module.css'
+import { eventData } from 'api/mock'
+import Event from 'components/Event'
+import classes from 'styles/events.module.css'
 
 export default function Events() {
   return (
-    <main className={classes.aboutMain}>
-      <div className={classes.root}>
-        <p>Events</p>
+    <main className={classes.main}>
+      <div className={classes.container}>
+        <h2 className={classes.header}>Upcoming Events</h2>
+        <div className={classes.eventContainer}>
+          {eventData.map((item, i) => (
+            <Event {...item} key={i} />
+          ))}
+        </div>
       </div>
     </main>
   )
