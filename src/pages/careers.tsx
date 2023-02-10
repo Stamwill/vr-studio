4336aa47274c668cbf7a7eeb609bf9b3a2cceea0
William Stam <stam@williams-mbp.home>
2023-02-10 06:42:28 +0000

    [careers] Map career data

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
+import { careerData } from 'api/mock'
+import Career from 'components/Career'
 import classes from 'styles/careers.module.css'
 
 export default function Careers() {
   return (
     <main className={classes.main}>
       <div className={classes.root}>
-        <p>Careers</p>
+        {careerData.map((item, idx) => (
+          <Career {...item} key={idx} />
+        ))}
       </div>
     </main>
   )
