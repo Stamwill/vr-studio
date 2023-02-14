9b28801931524f5ebd90e0259133bda7653ab8a5
William Stam <williamstam@gmail.com>
2023-02-14 11:45:52 +0000

    [products] Remove main

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -3,10 +3,10 @@ import classes from 'styles/products.module.css'
 
 export default function Products() {
   return (
-    <main className={classes.main}>
+    <div className={classes.main}>
       <div className={classes.root}>
         <p>Products</p>
       </div>
-    </main>
+    </div>
   )
 }
